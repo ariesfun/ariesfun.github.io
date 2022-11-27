03e09800c2ef26e450b3aff6c59f228fb48348cb
Ariesfun <2018351840@qq.com>
2022-11-27 09:52:28 +0800

    Site updated: 2022-11-27 09:52:27

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "CCCCCC",
+    "tmpSize": "14",
+    "cityColor": "D9D9D9",
+    "citySize": "14",
+    "aqiColor": "D9D9D9",
+    "aqiSize": "14",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "zh",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "509d5ea64019446288e41592506d4b79"
+  }
+}
